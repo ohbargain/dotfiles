63bd25ba5aec5540d4e9ac06c267824f9b3029d1
Seil Oh <hardboiledbaby@gmail.com>
2014-04-30 10:44:28 +0900

    fixed typo

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ fi
 
 VUNDLE_DIR="_vim/bundle/vundle"
 if [ ! -d "$VUNDLE_DIR" ]; then
-    git clone https://github.com/gmarik/Vundle.vim.g $VUNDLE_DIR
+    git clone https://github.com/gmarik/Vundle.vim $VUNDLE_DIR
     vim +PluginInstall +qall!
 else 
     cd $VUNDLE_DIR
